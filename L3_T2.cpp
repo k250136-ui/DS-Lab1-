dcce37e8b72dad512e5a0c3429978dddf56e6687
Hamdan Khan <hamdankhan28feb@gmail.com>
2026-09-01 12:36:54 +0500

    lab 3 task 2

---
+++ L3_T2.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+
+void ascending(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter number of employee scores: ";
+    cin >> n;
+
+    int scores[100];
+
+    cout << "Enter employee scores: ";
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> scores[i];
+    }
+
+    ascending(scores, n);
+
+    cout << "Scores in ascending order: ";
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << scores[i] << " ";
+    }
+
+    int k;
+    cout << "\nEnter the value of k: ";
+    cin >> k;
+    int kthScore = scores[k - 1];
+    cout << k << "th smallest score is: " << kthScore << endl;
+
+    int low = 0;
+    int high = n - 1;
+    bool found = false;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        if (scores[mid] == kthScore)
+        {
+            found = true;
+            break;
+        }
+        else if (kthScore < scores[mid])
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+
+    if (found)
+    {
+        cout << "Score exists in the array.";
+    }
+    else
+    {
+        cout << "Score does not exist in the array.";
+    }
+
+    return 0;
+}
